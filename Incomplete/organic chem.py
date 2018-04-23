d8413d8a3ccd5bf15b28507df76417bbd93e4c22
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-04-23 17:19:12 +0800

    Update organic chem.py

--- Incomplete/organic chem.py
+++ Incomplete/organic chem.py
@@ -1,85 +1,82 @@
-alkanes = ["0","methan","ethan","propan","butan","pentan","hexan","heptan","octan"]
-alkenes = ["0",'methen','ethen','propen','buten','penten','hexen','hepten','octen']
+import turtle
+import drawing_module
+
+scale = [43.3,25]
+wn = turtle.Screen()
+pen = turtle.Turtle()
+pen.penup()
+pen.ht()
+pen.speed(100)
+
+chains = ["0","meth","eth","prop","but","pent","hex","hept","oct"]
+suffixes = ["oic acid","enitrile","one","ol","al","cyclo","ene"]
 prefixes = ['methyl','ethyl','propyl','fluoro','chloro','bromo','iodo','hydroxy']
 numprefixes = ["0","mono","di","tri","tetra"]
-suffixes = ["oic acid","enitrile","one","ol","al","cyclo",]
-lists = [alkanes,alkenes,prefixes,numprefixes,suffixes]
+lists = [chains,suffixes,prefixes,numprefixes]
 
-def Check(inword,lists):
+def Type(inword):
+    try:
+        pos = int(inword)
+        return True
+    except ValueError:
+        return False
+
+def Groups(inword,lists):
     found = []
+    outword = inword
     for i in lists:
         for j in i:
             if j in inword:
                 found.append(j)
+                outword = outword.replace(j,str(found.index(j)))
                 inword = inword.replace(j,"")
-    return(found)
+    return {"groups":found,"pos":outword}
 
-
-
-print(Check("2-bromo,3-chloro pentane",lists))
-
-
-
-def Remove(phrase,chars):
-    out = ""
-    for j in phrase:
-        if j in chars:
-            phrase = phrase.replace(j,'')
-    return phrase
-
-def ReadInput(inword):
-    pos = []
-    groups = []
-    for i,j in enumerate(inword):
-        try:
-            posint = int(j)
-            inword = Remove(inword,j)
-            pos.append(posint)
-        except ValueError:
-            "do nothing"
-        if j in [" ",","]:
-            line = Remove(inword[0:i-1],",- ")
-            groups.insert(-1,line)
-    
-    main = Remove(inword,",- ")
-    for i in groups:
-        main = main.replace(i,"")
-    return {"positions":pos,"groups":groups,"main":main}
-
-def GroupCount(info):
-    positions = info["positions"]
+def GroupPos(inword,info):
     groups = info["groups"]
-    med = []
-    gcount = []
-    for i in groups:
-        scount = 0
-        for j in numprefixes:
-            if j in i:
-                med.append(numprefixes.index(j))
-            else:
-                scount += 1
-        if scount == 8 and i != '':
-            med.append(1)
-    csum = 0
-    for i in range(len(med)):
-        if len(positions) < csum+med[i]:
-            break
-        else:
-            csum += med[i]
-            gcount.append(med[i])
-    return gcount
+    pos = info["pos"]
+    endpos = pos
+    grouppositions = []
+    for i,j in enumerate(pos):
+        if j == "-" and Type(pos[i-1]) and pos[i-1] != "0":
+            grouppos = [pos[i-1],groups[int(pos[i+1])]]
+            grouppositions.append(grouppos)
+            endpos = endpos.replace(pos[i-1:i+2],"")
+    for i in endpos:
+        if Type(i):
+            main = groups[int(i)]
+    return {"groups":grouppositions,"main":main}
 
-def Compound(info):
-    positions = info["positions"]
-    groups = info["groups"]
-    main = groups[-1]
-    for i in range(len(alkanes)):
-        if alkanes[i] in main:
-            group = alkanes[i]
-            gnum = i
-        elif alkenes[i] in main:
-            group = alkenes[i]
-            gnum = 1
-    main = main.replace(group,'')
-    fgroup = suffixes[suffixes.index(main)]
-    return {"text":[group,fgroup],"number":gnum}
+def Draw(startpos,angle,length):
+    pen.setpos(startpos)
+    pen.seth(angle)
+    pen.pendown()
+    pen.left(30)
+    for i in range(length-1):
+        pen.forward(50)
+        if i%2 == 0:
+            pen.right(120)
+        pen.left(60)
+    pen.penup()
+
+while True:
+    inword = input("Enter name: ")
+    info = Groups(inword,lists)
+    if "-" not in inword:
+        final = {"groups":[['1',info["groups"][1]]],"main":info["groups"][0]}
+    else:
+        final = GroupPos(inword,info)
+    print(final)
+
+Draw([0,0],0,chains.index(final["main"]))
+for i in final["groups"]:
+    if final["groups"].index(i)%2 == 0:
+        direct = 60
+        side = 25
+    else:
+        direct = 240
+        side = 0
+    Draw([(int(i[0])-1)*43.3,side],direct,2)
+    if i[1] == "chloro":
+        drawing_module.c(20,[(int(i[0])-1)*43.3,side+60],"black",1)
+        drawing_module.l(20,[(int(i[0])-1)*43.3+10,side+60],"black",1)
