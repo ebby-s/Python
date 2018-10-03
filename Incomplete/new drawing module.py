b09f71334726618e25b63b4648c65f6b99d30721
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-10-03 20:15:54 +0800

    Add files via upload

--- Incomplete/new drawing module.py
+++ Incomplete/new drawing module.py
@@ -1,5 +1,6 @@
 import turtle
 
+colors = ["red","pink","violet","purple","blue","cyan","light green","yellow","orange"]
 screen = turtle.Screen()
 screen.bgcolor("black")
 
@@ -8,15 +9,15 @@ def draw(color):
     pencil.speed(100)
     pencil.ht()
     pencil.pencolor(color)
-    for i in range(90):
+    for i in range(45):
         pencil.pendown()
         pencil.forward(200)
         pencil.right(30)
         pencil.forward(80)
         pencil.penup()
         pencil.setpos([0,0])
-        pencil.seth(4*(1+i))
+        pencil.seth(8*(1+i))
 
 while __name__ == "__main__":
-    draw("red")
-    draw("black")
+    for color in colors:
+        draw(color)
