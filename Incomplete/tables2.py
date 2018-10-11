9ddd3ab43d218f8887ef69cf6b1d87c2a8873084
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-10-11 18:15:32 +0800

    Add files via upload

--- Incomplete/tables2.py
+++ Incomplete/tables2.py
@@ -1,13 +1,20 @@
+import operator
+
 class table:
 
     def __init__(self,attributes):
-        self.attributes = attributes
+        self.attributes = []
+        self.types = []
+        for attribute in attributes:
+            self.attributes.append(attribute[0])
+            self.types.append(attribute[1])
         self.data = []
+        self.operators = {"==":operator.eq,"<":operator.lt,">":operator.gt}
 
     def add_record(self,record):
         for i,field in enumerate(record):
-            if self.attributes[i][1] != type(field):
-                if type(field) == type(0) and self.attributes[i][1] == type(0.0):
+            if self.types[i] != type(field):
+                if type(field) == type(0) and self.types[i] == type(0.0):
                     record[i] = float(field)
                 else:
                     return False
@@ -26,19 +33,30 @@ class table:
 
     def search(self,target_conditions):
         conditions = []
-        operators = [" = "," > "," < "]
         for condition in target_conditions:
-            
-
-        output = self.data
-
+            condition = condition.split(" ")
+            condition[0] = self.attributes.index(condition[0])
+            condition[1] = self.operators[condition[1]]
+            try:
+                if self.types[condition[0]] == type(0):
+                    condition[2] = int(condition[2])
+                else:
+                    condition[2] = float(condition[2])
+            except:
+                if condition[1] != self.operators["=="]:
+                    continue
+            conditions.append(condition)
+        
+        output = []
         for condition in conditions:
-            for record in output:
-                if record
-
+            for i,record in enumerate(self.data):
+                if condition[1](record[condition[0]],condition[2]):
+                    output.append(record)
+        return output
 
 test = table([["name",type("a")],["age",type(0)],["height",type(0.0)]])
 test.add_record(["Ebby",17,1.5])
 test.add_record(["Sahil",17,1.6])
 test.add_record(["Edmund",19,2.5])
 test.add_record(["Jeff",21,3.5])
+print(test.search(["name == Sahil","age > 17"]))
