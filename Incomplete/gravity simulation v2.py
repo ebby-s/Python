c6dae2026e821c8ff73d084e3122b049ad39eb17
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-11-06 16:47:53 +0800

    Add files via upload

--- Incomplete/gravity simulation v2.py
+++ Incomplete/gravity simulation v2.py
@@ -1,9 +1,7 @@
-# A game about gravity
 import pygame, sys, time, math, random
 from pygame.locals import *
 pygame.init()
 fpsClock=pygame.time.Clock()
-#pygame.key.set_repeat(10,10)
 global G
 global resolution
 G = 0.001
@@ -11,10 +9,11 @@ resolution = (1600,900)
 main_font = pygame.font.SysFont('Courier',18,True)
 
 class Particle:
-
-    def __init__(self,mass=100):
+    
+    def __init__(self,contents=[100,0],density=1):
         """A massive object"""
-        self.mass = mass
+        self.contents = contents
+        self.mass = sum(self.contents)
         self.density = 2
         self.pos = [800,450]
         self.velocity = [0,0]
@@ -31,29 +30,31 @@ class Particle:
         self.velocity[1] += self.force[1]/self.mass
         self.pos[0] += self.velocity[0]
         self.pos[1] += self.velocity[1]
+        self.mass = sum(self.contents)
+        self.density = (1*self.contents[0]+0.2*self.contents[1])/self.mass
+        '''
         if self.mass < 1000:
-            #self.color = (150,150,140)
+            self.color = (150,150,140)
             self.density = 1
         elif 1000 <= self.mass < 5000:
-            #self.color = (73,255,237)
+            self.color = (73,255,237)
             self.density = 0.5
         elif self.mass > 5000:
-            #self.color = (10,10,10)
+            self.color = (20,20,20)
             self.density = 5
-        
+        '''
 
     def attract(self,other):    # Calculate forces due to gravity from other particles
-        if self != other:
-            dx = other.pos[0] - self.pos[0]
-            dy = other.pos[1] - self.pos[1]
-            r = (dy**2+dx**2)**0.5
-            magnitude = G*(self.mass*other.mass)/r**2
-            xmagnitude = (dx/r)*magnitude
-            ymagnitude = (dy/r)*magnitude
-            self.velocity[0] += xmagnitude/self.mass
-            self.velocity[1] += ymagnitude/self.mass
-            other.velocity[0] -= xmagnitude/other.mass
-            other.velocity[1] -= ymagnitude/other.mass
+        dx = other.pos[0] - self.pos[0]
+        dy = other.pos[1] - self.pos[1]
+        r = (dy**2+dx**2)**0.5
+        magnitude = G*(self.mass*other.mass)/r**2
+        xmagnitude = (dx/r)*magnitude
+        ymagnitude = (dy/r)*magnitude
+        self.velocity[0] += xmagnitude/self.mass
+        self.velocity[1] += ymagnitude/self.mass
+        other.velocity[0] -= xmagnitude/other.mass
+        other.velocity[1] -= ymagnitude/other.mass
 
     def collide(self,other):    # Check if there is a collision
         r1 = self.radius
@@ -81,29 +82,29 @@ def border(particles):        # Bounce partieles off border at 80% initial veloc
 def collision_detect(particles):
     for i,particle in enumerate(particles):
         for other in particles[i+1:]:
-            if particle != other:
-                if particle.collide(other):
-                    xmomentum = particle.mass*particle.velocity[0] + other.mass*other.velocity[0]
-                    ymomentum = particle.mass*particle.velocity[1] + other.mass*other.velocity[1]
-                    mass = particle.mass + other.mass
-                    if particle.mass >= other.mass:
-                        particle.mass = mass
-                        particle.velocity[0] = xmomentum/mass
-                        particle.velocity[1] = ymomentum/mass
-                        if particles.index(other) in [0,1]:
-                            particles.insert(particles.index(other)+1,Particle(100))
-                            particles[particles.index(other)+1].pos[0] = random.randint(resolution[0]*0.1,resolution[0]*0.9)
-                            particles[particles.index(other)+1].pos[1] = random.randint(resolution[1]*0.1,resolution[1]*0.9)
-                        del(particles[particles.index(other)])
-                    else:
-                        other.mass = mass
-                        other.xvelocity = xmomentum/mass
-                        other.yvelocity = ymomentum/mass
-                        if particles.index(particle) in [0,1]:
-                            particles.insert(particles.index(particle)+1,Particle(100))
-                            particles[particles.index(particle)+1].pos[0] = random.randint(resolution[0]*0.1,resolution[0]*0.9)
-                            particles[particles.index(particle)+1].pos[1] = random.randint(resolution[1]*0.1,resolution[1]*0.9)
-                        del(particles[particles.index(particle)])
+            if particle.collide(other):
+                xmomentum = particle.mass*particle.velocity[0] + other.mass*other.velocity[0]
+                ymomentum = particle.mass*particle.velocity[1] + other.mass*other.velocity[1]
+                mass = particle.mass + other.mass
+                contents = [particle.contents[0]+other.contents[0],particle.contents[1]+other.contents[1]]
+                if particle.mass >= other.mass:
+                    particle.contents = contents
+                    particle.velocity[0] = xmomentum/mass
+                    particle.velocity[1] = ymomentum/mass
+                    if particles.index(other) in [0,1]:
+                        particles.insert(particles.index(other)+1,Particle())
+                        particles[particles.index(other)+1].pos[0] = random.randint(resolution[0]*0.1,resolution[0]*0.9)
+                        particles[particles.index(other)+1].pos[1] = random.randint(resolution[1]*0.1,resolution[1]*0.9)
+                    del(particles[particles.index(other)])
+                else:
+                    other.contents = contents
+                    other.xvelocity = xmomentum/mass
+                    other.yvelocity = ymomentum/mass
+                    if particles.index(particle) in [0,1]:
+                        particles.insert(particles.index(particle)+1,Particle())
+                        particles[particles.index(particle)+1].pos[0] = random.randint(resolution[0]*0.1,resolution[0]*0.9)
+                        particles[particles.index(particle)+1].pos[1] = random.randint(resolution[1]*0.1,resolution[1]*0.9)
+                    del(particles[particles.index(particle)])
 
 screen=pygame.display.set_mode(resolution,0,32)
 
@@ -114,25 +115,14 @@ for particle in particles:
 particles[0].color = (0,36,255)
 particles[1].color = (36,255,0)
 
-'''
-particles[0].mass = 10000
-particles[1].mass = 2000
-particles[0].x = resolution[0]*0.5
-particles[0].y = resolution[1]*0.5
-particles[1].x = resolution[0]*0.5+100
-particles[1].y = resolution[1]*0.5
-particles[1].yvelocity = 0.42
-particles[0].yvelocity = -0.08
-'''
-
 while True:
     screen.fill((0,0,0))
 
     while len(particles) < 30:
-        particles.append(Particle(random.randint(40,min([particles[0].mass,particles[1].mass]))))
+        particles.append(Particle([random.randint(10,50),random.randint(1,5)]))
         particles[-1].pos[0] = random.randint(resolution[0]*0.1,resolution[0]*0.9)
         particles[-1].pos[1] = random.randint(resolution[1]*0.1,resolution[1]*0.9)
-
+    
     particles[0].color = (0,36,255)
     particles[1].color = (36,255,0)
 
@@ -141,13 +131,14 @@ while True:
         particle.draw(screen)
 
     border(particles)
+    
     collision_detect(particles)
 
     for i,particle in enumerate(particles):
         for other in particles[i+1:]:
             particle.attract(other)
 
-    for i,text in enumerate(["Player 1 mass: "+str(particles[0].mass),"Player 2 mass: "+str(particles[1].mass)]):
+    for i,text in enumerate(["Player 1 mass: "+str(particles[0].contents),"Player 2 mass: "+str(particles[1].contents)]):
         screen.blit(main_font.render(text,True,(255,255,255)),(10,10+30*i))
 
     for event in pygame.event.get():
