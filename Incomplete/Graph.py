9241f4f0fa06bbbbbcd357c5eaa99d0ddf95070f
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-12-09 16:42:37 +0800

    Update Graph.py

--- Incomplete/Graph.py
+++ Incomplete/Graph.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 X = [0,1,1,2,4,5,7,9,11,13,15]
 Y = [0,810,1610,2420,3230,4030,4840,5640,6450,7260,8060]
 
@@ -14,8 +16,33 @@ def best_fit(X, Y):
 
 a, b = best_fit(X, Y)
 
-import matplotlib.pyplot as plt
 plt.scatter(X, Y)
 yfit = [a + b * xi for xi in X]
 plt.plot(X, yfit)
 plt.show()
+
+'''
+import scipy.optimize as optimization
+import numpy
+
+def func(x,a,b):
+    return a+b*x
+
+xs = numpy.array([0,1,1,2,4,5,7,9,11,13,15])
+ys = numpy.array([0,810,1610,2420,3230,4030,4840,5440,6450,7260,8060])
+x0    = numpy.array([0.0, 0.0, 0.0])
+sigma = numpy.array([1.0,1.0,1.0,1.0,1.0,1.0])
+
+plt.xlabel('Change in T')
+plt.ylabel('Change in Q')
+plt.plot(xs,ys, 'ro')
+plt.axis([0, 16, 0, 8500])
+
+x = numpy.linspace(0,16,100)
+y = 430*x+1600
+plt.plot(x, y, '-r', label='y=2x+1')
+
+plt.show()
+#print(optimization.curve_fit(func, xs, ys, x0, sigma))
+input()
+'''
