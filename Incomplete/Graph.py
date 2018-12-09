4fea32a8ba1721fcdcbfe3e6b29fcea781d4ee49
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-12-09 18:31:59 +0800

    Update Graph.py

--- Incomplete/Graph.py
+++ Incomplete/Graph.py
@@ -9,18 +9,18 @@ def best_fit(X, Y):
     b = numer / denum
     a = ybar - b * xbar
     print('best fit line:\ny = {:.2f} + {:.2f}x'.format(a, b))
-    return a, b
+    return a,b
 
 X = [0,1,1,2,4,5,7,9,11,13,15]
 Y = [0,810,1610,2420,3230,4030,4840,5640,6450,7260,8060]
-a, b = best_fit(X, Y)
+a,b = best_fit(X, Y)
 
 plt.xlabel('ΔT/K')
 plt.ylabel('ΔQ/J')
 plt.title('Specific heat capacity of Cu')
-plt.scatter(X, Y)
-yfit = [a + b * xi for xi in X]
-plt.plot(X, yfit)
+plt.scatter(X,Y)
+yfit = [a+b*xi for xi in X]
+plt.plot(X,yfit)
 plt.show()
 input()
 
